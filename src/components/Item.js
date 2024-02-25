45965f1cd69301dcd0f71a843d64295b900b6722
Xennia Contreras <Xenbydesign@gmail.com>
2024-02-25 00:49:56 -0800

    done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  function handleCartToggle(){
+    setInCart((prevInCart)=> !prevInCart);
+  }
+  const itemClass = inCart ? "in-cart" : "";
+  const buttonText = inCart ? "Remove From Cart" : "Add to Cart"
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCartToggle}>{buttonText}</button>
     </li>
   );
 }
